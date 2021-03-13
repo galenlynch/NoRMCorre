f05ac21cd5ec49b7bf3faa409e5a0a93daed6783
Galen Lynch <galen@galenlynch.com>
2021-03-13 10:03:48 -0500

    Fix bugs in glynch_1p

--- glynch_1p.m
+++ glynch_1p.m
@@ -43,14 +43,16 @@ Y = imfilter(Yf, psf, 'symmetric');
 outf = fullfile(writedir, sprintf('%s_motion_corrected.mat', file_name))
 %% first try out rigid motion correction
     % exclude boundaries due to high pass filtering effects
-options_r = NoRMCorreSetParms('d1', nx - bound, 'd2', ny - bound, ...
+options_r = NoRMCorreSetParms('d1', nx - Options.bound, ...
+                              'd2', ny - Options.bound, ...
                               'bin_width', Options.bin_width, ...
                               'max_shift', Options.max_shift, ...
                               'iter', Options.iter, ...
                               'correct_bidir', Options.correct_bidir);
 
 %% register using the high pass filtered data and apply shifts to original data
-roi = Y(bound/2+1 : end-bound/2, bound/2+1 : end-bound/2, :);
+roi = Y(Options.bound/2+1 : end-Options.bound/2, ...
+        Options.bound/2+1 : end-Options.bound/2, :);
 [M1, shifts1, template1] = normcorre_batch(roi, options_r); % register filtered data
     % exclude boundaries due to high pass filtering effects
 
@@ -58,7 +60,7 @@ roi = Y(bound/2+1 : end-bound/2, bound/2+1 : end-bound/2, :);
 % name that does not exist. Change options_r.tiff_filename
 % or options_r.h5_filename accordingly.
 
-Mr = apply_shifts(Yf, shifts1, options_r, bound/2, bound/2);
+Mr = apply_shifts(Yf, shifts1, options_r, Options.bound/2, Options.bound/2);
 
 save(outf, 'Mr', 'M1', 'shifts1', 'template1', 'options_r', '-v7.3');
 
